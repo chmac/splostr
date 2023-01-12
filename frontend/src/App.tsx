263866e51a301288d84f04d7cbbd89399f54eaa3
Callum Macdonald <git@callum-macdonald.com>
2023-01-12 11:57:18 +0100

    Add app bar.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
 import { Container, Typography } from "@mui/material";
 import { Link, Outlet, Route, Routes } from "react-router-dom";
+import Bar from "./scenes/Bar/Bar.scene";
 
-export default function App() {
+const App = () => {
   return (
     <Container maxWidth="lg">
-      <Typography variant="h2">Basic Example</Typography>
-
-      <Typography>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </Typography>
-
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
@@ -31,31 +22,14 @@ export default function App() {
       </Routes>
     </Container>
   );
-}
+};
+
+export default App;
 
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
+      <Bar />
 
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
