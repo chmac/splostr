6064b20bdc835f7e72d18af14588b7f9aa15dd41
Callum Macdonald <git@callum-macdonald.com>
2023-01-16 11:40:25 +0100

    Remove testing cruft

--- frontend/src/scenes/Home/Home.scene.tsx
+++ frontend/src/scenes/Home/Home.scene.tsx
@@ -1,42 +1,11 @@
 import { Button, Paper, Typography } from "@mui/material";
-import { useNostr } from "nostr-react";
 import { getPublicKey } from "nostr-tools";
 import { PRIVATE_KEY } from "../../app/constants";
-import { useNostrPublish, useNostrQuery } from "../../nostr-redux";
+import { useNostrPublish } from "../../nostr-redux";
 import { createProfileUpdateEvent } from "../../services/nostr/nostr.service";
-
-const Testing = () => {
-  const profileEvents = useNostrQuery({
-    authors: [
-      "b1919f284056c3b2d81b35a8664a5cefa9794f43eb9b5c288865674a88d05664",
-      "3e5dc1f1ed0ad1bd24055da9588a69d37d6eb983526c0fb12aff31199ed8f4b0",
-    ],
-    kinds: [0],
-  });
-
-  return (
-    <Paper>
-      <Typography>Nested Testing</Typography>
-      <ul>
-        {profileEvents.map((event) => (
-          <Typography component="li" key={event.id}>
-            {JSON.stringify(event)}
-          </Typography>
-        ))}
-      </ul>
-    </Paper>
-  );
-};
 
 export const Home = () => {
   const { publish, result } = useNostrPublish();
-  const profileEvents = useNostrQuery({
-    authors: [
-      "b1919f284056c3b2d81b35a8664a5cefa9794f43eb9b5c288865674a88d05664",
-      "3e5dc1f1ed0ad1bd24055da9588a69d37d6eb983526c0fb12aff31199ed8f4b0",
-    ],
-    kinds: [0],
-  });
 
   return (
     <div>
@@ -45,19 +14,6 @@ export const Home = () => {
         <Paper>
           <Typography>{getPublicKey(PRIVATE_KEY)}</Typography>
         </Paper>
-
-        <Paper>
-          <Typography>Testing</Typography>
-          <ul>
-            {profileEvents.map((event) => (
-              <Typography component="li" key={event.id}>
-                {JSON.stringify(event)}
-              </Typography>
-            ))}
-          </ul>
-        </Paper>
-
-        <Testing />
 
         <Button
           onClick={() => {
