99d53b33105b87b0a1ee286d48ed9cf8d59cb71e
Callum Macdonald <git@callum-macdonald.com>
2023-02-17 11:27:50 +0100

    Cleanup

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import Group from "$lib/scenes/Group.svelte";
   import Home from "$lib/scenes/Home.svelte";
   import Router from "svelte-spa-router";
-  // import { SvelteComponent } from "svelte";
 
   const routes = {
     "/": Home,
