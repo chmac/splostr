70c1d62628b168ab6d6c061b6cfb96c1114b4d3c
Callum Macdonald <git@callum-macdonald.com>
2023-02-17 11:27:20 +0100

    Basic hash router that seems to work.
    The `ssr = false` on the layout is essential, as svelte-spa-router won't work on the server.

---
+++ frontend/src/lib/scenes/Group.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let params = { id: "" };
+</script>
+
+<h1>Group</h1>
+<p>Group id is {params.id}</p>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script type="ts">
+  import Group from "$lib/scenes/Group.svelte";
+  import Home from "$lib/scenes/Home.svelte";
+  import Router from "svelte-spa-router";
+  // import { SvelteComponent } from "svelte";
+
+  const routes = {
+    "/": Home,
+    "/group/:id": Group,
+  };
+</script>
+
+<div>
+  <Router {routes} />
+</div>
